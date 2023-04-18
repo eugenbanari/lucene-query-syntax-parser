1f840d02d7a661c771e2bbccdb6ba8ab061484b4
Eugen Banari <8667298+eugenbanari@users.noreply.github.com>
2023-04-18 21:04:10 +0300

    Update index.js

--- example/index.js
+++ example/index.js
@@ -4,4 +4,5 @@ const querystring = 'title:"foo bar" AND body:"quick fox"';
 const abstractSyntaxTree = luceneQuerySyntaxParser.parse(querystring);
 const result = JSON.stringify(abstractSyntaxTree, null, 4);
 
-console.log(result);
+console.log('Query: ', querystring);
+console.log('Generated abstract syntax tree: \n', result);
